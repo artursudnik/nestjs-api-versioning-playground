0d88d21eb6325d6b51bd15970cd85b9b56b44956
Artur Sudnik-Hrynkiewicz <artur.sudnik@gmail.com>
2022-07-04 16:01:40 +0200

    enable swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DefaultResponseHeadersInterceptor } from './interceptors/default-response-headers.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new DefaultResponseHeadersInterceptor());
 
+  const config = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
